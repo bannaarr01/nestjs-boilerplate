199c8e78404676408cae96005036bbed9d220311
B. Joshua Adedigba <tbannaarr@gmail.com>
2026-03-03 06:56:40 +0800

    chore: health check dto

---
+++ src/app/dto/health-check.dto.ts
@@ -0,0 +1,14 @@
+import { ApiProperty } from '@nestjs/swagger';
+
+export class HealthCheckDto {
+  @ApiProperty({ example: 'ok' })
+  status: string;
+
+  @ApiProperty({ example: '2026-03-02T12:00:00.000Z' })
+  timestamp: string;
+
+  @ApiProperty({ example: 123.45 })
+  uptime: number;
+}
+
+
