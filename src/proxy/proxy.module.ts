04bb83f4e058e4c9665cab5f55c3222e3050bf61
B. Joshua Adedigba <tbannaarr@gmail.com>
2026-03-03 06:54:17 +0800

    feat: setup proxy service to handshake to external services and transform result

---
+++ src/proxy/proxy.module.ts
@@ -0,0 +1,14 @@
+import { Module } from '@nestjs/common';
+import { HttpModule } from '@nestjs/axios';
+import { ProxyService } from './proxy.service';
+import { CommonModule } from '../common/common.module';
+import { ProxyConfigService } from './proxy-config.service';
+
+@Module({
+  imports: [HttpModule, CommonModule],
+  providers: [ProxyConfigService, ProxyService],
+  exports: [ProxyConfigService, ProxyService]
+})
+export class ProxyModule {}
+
+
